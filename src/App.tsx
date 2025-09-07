2305c0dcb5f5db8449e2502edb9992e58c3ad45f
mile09 <tmtmosweu@gmail.com>
2025-09-07 18:00:03 +0200

    Add Nunito and Exo 2 fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div className="App container p-4">
-      <h1>The Scientific Officer</h1>
+      <h1 className="fs-6xl fw-bold">The Scientific Officer</h1>
 
       <p className="fs-lg">Welcome to The Scientific Officer application!</p>
     </div>
