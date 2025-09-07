bdd2267205ac4bb7ddfcb7325baa42141d339ca6
mile09 <tmtmosweu@gmail.com>
2025-09-07 19:36:18 +0200

    Add React Router for navigation and implement layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { Routes, Route } from 'react-router'
+import Layout from './layout'
+
 const App = () => {
   return (
-    <div className="App container p-4">
-      <h1 className="fs-6xl fw-bold">The Scientific Officer</h1>
-
-      <p className="fs-lg">Welcome to The Scientific Officer application!</p>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<div>Home Page</div>} />
+          <Route path="/practice-questions" element={<div>Practice Questions Page</div>} />
+          <Route path="/mock-test" element={<div>Mock Test Page</div>} />
+          <Route path="/resources" element={<div>Resources Page</div>} />
+          <Route path="/tips-and-strategies" element={<div>Tips and Strategies Page</div>} />
+          <Route path="/about" element={<div>About Page</div>} />
+          <Route path="*" element={<div>404 Not Found</div>} />
+        </Route>
+      </Routes>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './assets/sass/index.scss'
+import { BrowserRouter } from 'react-router'
+
 import App from './App.tsx'
+
+import './assets/sass/index.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
