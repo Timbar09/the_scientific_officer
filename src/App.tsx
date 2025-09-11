b4eeef0c1cf1f5e31930d532dcdbbce417753bf0
mile09 <tmtmosweu@gmail.com>
2025-09-11 09:25:44 +0200

    Adjust color palette

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import { Routes, Route } from 'react-router'
-import Layout from './layout'
+import { Routes, Route } from "react-router";
+import Layout from "./layout";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="App bg-neutral-100 clr-neutral-500">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<div>Home Page</div>} />
-          <Route path="/practice-questions" element={<div>Practice Questions Page</div>} />
+          <Route
+            path="/practice-questions"
+            element={<div>Practice Questions Page</div>}
+          />
           <Route path="/mock-test" element={<div>Mock Test Page</div>} />
           <Route path="/resources" element={<div>Resources Page</div>} />
-          <Route path="/tips-and-strategies" element={<div>Tips and Strategies Page</div>} />
+          <Route
+            path="/tips-and-strategies"
+            element={<div>Tips and Strategies Page</div>}
+          />
           <Route path="/about" element={<div>About Page</div>} />
           <Route path="*" element={<div>404 Not Found</div>} />
         </Route>
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
