882072704961d86df09355728875ac0f1a733327
mile09 <tmtmosweu@gmail.com>
2025-09-13 16:10:23 +0200

    Refactor header component for improved responsiveness and accessibility; update color function to include opacity

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,54 +1,124 @@
 import { useState } from "react";
-import { Link } from "react-router";
+import { NavLink } from "react-router";
+
+import { AnimatePresence, motion } from "motion/react";
+
+import { useMediaQuery } from "../hooks/useMediaQuery";
+
+const navLinks = [
+  { name: "Home", path: "/" },
+  {
+    name: "Practice",
+    path: "/practice",
+  } /* Will add dropdown later (Practice-questions & Mock Test) */,
+  {
+    name: "Resources",
+    path: "/resources",
+  } /* Will add dropdown later (Books, Articles, Videos) */,
+  // { name: "Tips and Strategies", path: "/tips-and-strategies" },
+  { name: "About", path: "/about" },
+];
+
+interface MenuListProps {
+  isNavOpen: boolean;
+  isTablet: boolean;
+  handleMenuItemClick: () => void;
+}
+
+const MenuList = ({
+  isNavOpen,
+  isTablet,
+  handleMenuItemClick,
+}: MenuListProps) => (
+  <AnimatePresence>
+    {isNavOpen && !isTablet && (
+      <motion.ul
+        className="header__nav--menu flex flex-col"
+        role="menu"
+        id="menu"
+        initial={{ top: 0, right: "-100%" }}
+        animate={{ top: 0, right: 0 }}
+        exit={{ top: 0, right: "-100%" }}
+      >
+        {navLinks.map((link) => (
+          <li
+            key={link.path}
+            className="header__nav--menu__item p-1"
+            role="menuitem"
+            onClick={handleMenuItemClick}
+          >
+            <NavLink to={link.path} className="header__nav--menu__link fw-bold">
+              {link.name}
+            </NavLink>
+          </li>
+        ))}
+      </motion.ul>
+    )}
+  </AnimatePresence>
+);
+
+const NavList = () => (
+  <ul
+    className="header__nav--list flex gap-3"
+    role="navigation"
+    id="navigation-list"
+  >
+    {navLinks.map((link) => (
+      <li key={link.path} className="header__nav--list__item" role="listitem">
+        <NavLink
+          to={link.path}
+          className={({ isActive }) =>
+            `header__nav--list__link${isActive ? " active" : ""}`
+          }
+        >
+          {link.name}
+        </NavLink>
+      </li>
+    ))}
+  </ul>
+);
 
 const Header = () => {
-  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
+  const isTablet: boolean = useMediaQuery({ breakpoint: "md" });
 
-  const navLinks = [
-    { name: "Home", path: "/" },
-    { name: "Practice Questions", path: "/practice-questions" },
-    { name: "Mock Test", path: "/mock-test" },
-    { name: "Resources", path: "/resources" },
-    { name: "Tips and Strategies", path: "/tips-and-strategies" },
-    { name: "About", path: "/about" },
-  ];
+  const toggleNavMenu = () => {
+    setIsNavOpen(!isNavOpen);
+
+    if (!isNavOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  };
 
   return (
-    <header className="header p-3">
-      <div className="container flex ai-center jc-between">
+    <header className="header p-block-3" role="banner">
+      <div className="container p-inline-3 flex ai-center jc-between">
         <div className="header__logo" role="logo">
           Scientific Officer
         </div>
 
-        <nav
-          className={`header__nav ${isNavOpen ? "open" : "closed"}`}
-          role="navigation"
-        >
+        <nav className="header__nav" role="navigation">
           <button
             className="header__nav--toggle__button grid p-1"
             aria-label="Toggle navigation menu"
             aria-expanded={isNavOpen}
             aria-controls="navigation-list"
-            onClick={() => setIsNavOpen(!isNavOpen)}
+            onClick={toggleNavMenu}
           >
             <span className="material-symbols-outlined">
               {isNavOpen ? "close" : "menu"}
             </span>
           </button>
 
-          <ul
-            className="header__nav--list flex flex-col flex-@md-row ai-center gap-1"
-            role="navigation"
-            id="navigation-list"
-          >
-            {navLinks.map((link) => (
-              <li key={link.path} className="header__nav-item" role="listitem">
-                <Link to={link.path} className="header__nav-link">
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
+          <MenuList
+            isNavOpen={isNavOpen}
+            isTablet={isTablet}
+            handleMenuItemClick={toggleNavMenu}
+          />
+
+          {isTablet && <NavList />}
         </nav>
       </div>
     </header>
