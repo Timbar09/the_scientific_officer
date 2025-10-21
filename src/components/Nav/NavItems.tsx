db730fe1e30cf41c586d3fe978fe30e9356831e4
Miles Mosweu <tmtmosweu@gmail.com>
2025-10-21 12:24:57 +0200

    Separate  NavDropdown component into its own file

--- src/components/Nav/NavItems.tsx
+++ src/components/Nav/NavItems.tsx
@@ -3,7 +3,9 @@ import { NavLink } from "react-router";
 import { motion, AnimatePresence } from "motion/react";
 import type { Variants } from "motion/react";
 
-interface NavItem {
+import NavDropdown from "./NavDropdown";
+
+export interface NavItem {
   name: string;
   path: string;
   sub?: NavItem[];
@@ -122,78 +124,5 @@ const NavItem = ({
         </NavLink>
       )}
     </li>
-  );
-};
-
-interface NavDropdownProps {
-  subItems: NavItem[];
-  isMobileMenu: boolean;
-  motionVariants: Variants;
-  closeDropdown: () => void;
-}
-
-const NavDropdown = ({
-  subItems,
-  motionVariants,
-  isMobileMenu,
-  closeDropdown,
-}: NavDropdownProps) => {
-  return isMobileMenu ? (
-    <div key="modal" className="header__nav--list__dropdown p-block-1">
-      <ul
-        className="header__nav--list__dropdown--list p-block-2 p-inline-3"
-        role="menu"
-      >
-        {subItems.map((item) => (
-          <motion.li
-            key={item.path}
-            className="header__nav--list__item"
-            role="menuitem"
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            variants={motionVariants}
-          >
-            <NavLink
-              to={item.path}
-              className="header__nav--list__link"
-              onClick={closeDropdown}
-            >
-              {item.name}
-            </NavLink>
-          </motion.li>
-        ))}
-      </ul>
-    </div>
-  ) : (
-    <motion.div
-      key="modal"
-      className="header__nav--list__dropdown p-block-1"
-      initial="initial"
-      animate="animate"
-      exit="exit"
-      variants={motionVariants}
-    >
-      <ul
-        className="header__nav--list__dropdown--list p-block-2 p-inline-3"
-        role="menu"
-      >
-        {subItems.map((item) => (
-          <li
-            key={item.path}
-            className="header__nav--list__item"
-            role="menuitem"
-          >
-            <NavLink
-              to={item.path}
-              className="header__nav--list__link"
-              onClick={closeDropdown}
-            >
-              {item.name}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </motion.div>
   );
 };
