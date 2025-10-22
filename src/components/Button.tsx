c4ba6f30b6a57a669d0ed6b817389ee17989a0fd
Miles Mosweu <tmtmosweu@gmail.com>
2025-10-22 17:56:14 +0200

    Refactor Button component to support navigation via NavLink and enhance icon handling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,31 +1,60 @@
+import { NavLink } from "react-router";
+
 interface ButtonProps {
   type?: "button" | "submit" | "reset";
-  className?: string;
+  to?: string;
   variant?: "primary" | "secondary" | "tertiary";
-  onClick?: () => void;
   children?: React.ReactNode;
+  className?: string;
+  icon?: string;
+  iconPosition?: "left" | "right";
+  onClick?: () => void;
 }
 
 const Button = ({
   type = "button",
-  onClick,
-  children,
-  className,
+  to,
   variant = "primary",
+  children = `${variant.charAt(0).toUpperCase()}${variant.slice(1)} Button`,
+  className = "",
+  icon,
+  iconPosition = "left",
+  onClick,
 }: ButtonProps) => {
-  className = `btn btn__${variant} ${className || ""}`.trim();
+  const variantClasses = `btn btn__${variant}`;
+  const flexClasses = "flex ai-center gap-1";
+  const iconClasses = `${
+    icon
+      ? iconPosition.toLocaleLowerCase() === "left"
+        ? "p-block-1 p-inline-start-1 p-inline-end-4"
+        : "p-block-1 p-inline-start-4 p-inline-end-1 flex-row-reverse"
+      : "p-block-1 p-inline-3"
+  }`;
+  // const iconClasses = `${icon ? "p-block-1" : "p-block-1 p-inline-3"}`;
+  className =
+    `${variantClasses} ${className} ${flexClasses} ${iconClasses}`.trim();
 
-  if (children) {
+  if (to) {
     return (
-      <button type={type} className={className} onClick={onClick}>
+      <NavLink to={to} className={className} onClick={onClick}>
+        {icon && (
+          <span className="btn__icon grid">
+            <span className="material-symbols-outlined">{icon}</span>
+          </span>
+        )}
         {children}
-      </button>
+      </NavLink>
     );
   }
 
   return (
     <button type={type} className={className} onClick={onClick}>
-      Start Here!
+      {icon && (
+        <span className="btn__icon grid">
+          <span className="material-symbols-outlined">{icon}</span>
+        </span>
+      )}
+      {children}
     </button>
   );
 };
