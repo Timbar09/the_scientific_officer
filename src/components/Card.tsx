61766dc85896f4f9c00bac1d1e1bc6d33cb46abf
Miles Mosweu <tmtmosweu@gmail.com>
2026-01-13 15:10:11 +0200

    Add Card component with responsive design and icon support

---
+++ src/components/Card.tsx
@@ -0,0 +1,36 @@
+import type { IconProps } from "./Button";
+
+interface CardProps {
+  size?: "small" | "medium";
+  icon?: IconProps;
+  title?: string;
+  text?: string;
+}
+
+const Card = ({ icon, title, text, size = "medium" }: CardProps) => {
+  const flexDirectionClass = size === "medium" ? "flex-col" : "flex-row";
+
+  return (
+    <div
+      className={`card card__${size} flex ${flexDirectionClass} ai-start gap-3`}
+    >
+      {icon && (
+        <span className={`card__icon card__${size}--icon grid`}>
+          <span className="material-symbols-outlined">{icon.name}</span>
+        </span>
+      )}
+
+      <div className={`card__${size}--text flex flex-col`}>
+        {title && (
+          <h3 className={`card__title card__${size}--title`}>{title}</h3>
+        )}
+
+        {text && (
+          <p className={`card__text card__${size}--description`}>{text}</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
