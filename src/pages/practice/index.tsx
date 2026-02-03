0868d92861dab70c875787119de127ec48d98018
Miles Mosweu <tmtmosweu@gmail.com>
2026-02-03 13:40:11 +0200

    Add Field set for Practice topic check boxes in the  Practice page

--- src/pages/practice/index.tsx
+++ src/pages/practice/index.tsx
@@ -1,17 +1,50 @@
 // import UnderConstruction from "../../components/UnderConstruction";
 
 const Practice = () => {
+  const topics = [
+    { id: 1, name: "Animal Health", icon: "cardiology" },
+    { id: 2, name: "Animal Nutrition", icon: "nutrition" },
+    { id: 3, name: "Animal Breeding", icon: "genetics" },
+    { id: 4, name: "Animal Welfare", icon: "pets" },
+    { id: 5, name: "Animal Husbandry", icon: "cruelty_free" },
+  ];
+
   return (
     <div className="practice page">
       <div className="container">
         <h1 className="page__title">Let's Put Your Knowledge to the Test!</h1>
-
         <p className="page__description">
           Select from a variety of topics, difficulty levels, and question types
           to customize your practice sessions. Whether you're a beginner or an
           expert, our practice questions are designed to challenge and enhance
           your understanding of scientific concepts.
         </p>
+
+        <form className="practice__form m-block-start-5">
+          <fieldset className="practice__fieldset p-3">
+            <legend className="practice__legend">
+              Choose Topics You Want to cover:
+            </legend>
+            <div className="practice__option--list flex flex-wrap gap-2">
+              {topics.map(({ id, name, icon }) => (
+                <label key={id} className="practice__option--item grid">
+                  <input
+                    type="checkbox"
+                    name="topic"
+                    defaultChecked={id === 1}
+                    value={name.toLowerCase()}
+                  />{" "}
+                  <span className="practice__option--item__name p-block-2 p-inline-4">
+                    {name}
+                    <span className="material-symbols-outlined practice__option--item__icon">
+                      {icon}
+                    </span>
+                  </span>
+                </label>
+              ))}
+            </div>
+          </fieldset>
+        </form>
       </div>
     </div>
   );
