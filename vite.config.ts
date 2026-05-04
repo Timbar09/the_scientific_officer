9f0ddd160d2ce8820af522b8b1fe5ff4e80ccb91
Miles Mosweu <tmtmosweu@gmail.com>
2026-05-04 20:28:23 +0200

    Add vite-tsconfig-paths for improved path resolution and update tsconfig and vite config for aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@components": "/src/components",
+      "@pages": "/src/pages",
+      "@assets": "/src/assets",
+      "@utils": "/src/utils",
+    },
+  },
+});
