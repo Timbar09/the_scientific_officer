9d97b481ec29e956228c252ba8789e5c213f0ebc
Miles Mosweu <tmtmosweu@gmail.com>
2026-09-07 18:56:43 +0200

    Refactor PracticeSettingQuestionTypeSelection to simplify state management by removing useEffect and initializing selectedTypeId directly from defaultValue

--- src/pages/practice/settings/PracticeSettingQuestionTypeSelection.tsx
+++ src/pages/practice/settings/PracticeSettingQuestionTypeSelection.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import type { UseFormRegister, FieldValues } from "react-hook-form";
 import type { FormFieldData } from "../../../components/Form/types";
@@ -18,26 +18,8 @@ const PracticeSettingQuestionTypeSelection = ({
   register,
   formSectionData,
 }: QuestionTypeSelectionProps) => {
-  const { name, defaultValue, questionTypes } = formSectionData;
-
-  const [selectedTypeId, setSelectedTypeId] = useState<number>(
-    defaultValue === "all"
-      ? 0
-      : (questionTypes.find((questionType) => {
-          return questionType.name.toLowerCase() === defaultValue;
-        })?.id ?? 0),
-  );
-
-  useEffect(() => {
-    const nextSelectedTypeId =
-      defaultValue === "all"
-        ? 0
-        : (questionTypes.find((questionType) => {
-            return questionType.name.toLowerCase() === defaultValue;
-          })?.id ?? 0);
-
-    setSelectedTypeId(nextSelectedTypeId);
-  }, [defaultValue, questionTypes]);
+  const { name, questionTypes } = formSectionData;
+  const [selectedTypeId, setSelectedTypeId] = useState<number>(0);
 
   const label = {
     text: "Select Type of Questions",
@@ -47,7 +29,6 @@ const PracticeSettingQuestionTypeSelection = ({
   const allType = {
     id: 0,
     value: "all",
-    isChecked: selectedTypeId === 0,
   };
 
   const options = questionTypes
@@ -55,7 +36,6 @@ const PracticeSettingQuestionTypeSelection = ({
     .map(({ id, name }) => ({
       id,
       value: name.toLowerCase(),
-      isChecked: selectedTypeId === id,
     }));
 
   options.unshift(allType);
