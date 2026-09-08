f338f7e3f776b4bbaa012704a71f623b1e212dde
Miles Mosweu <tmtmosweu@gmail.com>
2026-09-08 12:33:23 +0200

    Refactor ProgressBar component to use motion for animations; separate into LinearProgressBar and SteppedProgressBar for improved structure and visual feedback

--- src/pages/practice/session/Header/ProgressBar.tsx
+++ src/pages/practice/session/Header/ProgressBar.tsx
@@ -1,3 +1,7 @@
+import { motion } from "motion/react";
+
+const BASE_CN = "practice__header--progress";
+
 const ProgressBar = ({
   unanswered,
   total,
@@ -11,50 +15,94 @@ const ProgressBar = ({
   const progressLabel = `${total - unanswered} out of ${total} answered`;
   const isStepped = variant === "stepped";
 
-  const baseCN = "practice__header--progress";
-  const typeBaseCN = `${baseCN}__${variant}`;
+  const typeBaseCN = `${BASE_CN}__${variant}`;
   const stepOrRailCN = isStepped ? "step" : "rail";
 
   return (
-    <div className={`${baseCN} ${typeBaseCN} flex gap-1 ai-center`}>
+    <div className={`${BASE_CN} ${typeBaseCN} flex gap-1 ai-center`}>
       <div
-        className={`${baseCN}__container ${typeBaseCN}--${stepOrRailCN}__container`}
+        className={`${BASE_CN}__container ${typeBaseCN}--${stepOrRailCN}__container`}
       >
         {variant === "linear" ? (
-          <div className={`${typeBaseCN}--rail`}>
-            <div
-              className={`${typeBaseCN}--rail__bar`}
-              style={{ width: `${progressPercentage}%` }}
-              aria-label={progressLabel}
-              data-progress={progressLabel}
-            ></div>
-          </div>
+          <LinearProgressBar
+            progressPercentage={progressPercentage}
+            progressLabel={progressLabel}
+          />
         ) : (
-          <>
-            <div className={`${typeBaseCN}--step__list flex jc-between`}>
-              {Array.from({ length: 50 }).map((_, index) => {
-                const stepPercentage = ((index + 1) / 50) * 100;
-                const isCompleted = stepPercentage <= progressPercentage;
-
-                const stepCN = isCompleted
-                  ? `${typeBaseCN}--step__item--completed`
-                  : "";
-
-                return (
-                  <div
-                    key={index}
-                    className={`${typeBaseCN}--step__item ${stepCN}`}
-                  />
-                );
-              })}
-            </div>
-          </>
+          <SteppedProgressBar progressPercentage={progressPercentage} />
         )}
       </div>
 
-      <div className={`${baseCN}__value`}>
+      <div className={`${BASE_CN}__value`}>
         {`${Math.round(progressPercentage)}%`}
       </div>
+    </div>
+  );
+};
+
+const SteppedProgressBar = ({
+  progressPercentage,
+}: {
+  progressPercentage: number;
+}) => {
+  const steps = 35;
+
+  const typeBaseCN = `${BASE_CN}__stepped`;
+
+  return (
+    <div className={`${typeBaseCN}--step__list flex jc-between`}>
+      {Array.from({ length: steps }).map((_, index) => {
+        const stepFraction = (index + 1) / steps;
+        const stepPercentage = stepFraction * 100;
+        const isCompleted = stepPercentage <= progressPercentage;
+
+        const cssVars = {
+          "--step-bg-opacity": Math.max(0.25, stepFraction), // Adjust the opacity based on the step fraction
+        } as React.CSSProperties;
+        const stepCN = isCompleted
+          ? `${typeBaseCN}--step__item--completed`
+          : "";
+
+        return (
+          <div key={index} className={`${typeBaseCN}--step__item--container`}>
+            <motion.div
+              style={cssVars}
+              className={`${typeBaseCN}--step__item ${stepCN}`}
+              animate={{
+                opacity: isCompleted ? 1 : 0,
+                scaleY: isCompleted ? 1 : 0.85,
+              }}
+              transition={{
+                duration: 0.5,
+                ease: "easeOut",
+              }}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const LinearProgressBar = ({
+  progressPercentage,
+  progressLabel,
+}: {
+  progressPercentage: number;
+  progressLabel: string;
+}) => {
+  const typeBaseCN = `${BASE_CN}__linear`;
+
+  return (
+    <div className={`${typeBaseCN}--rail`}>
+      <motion.div
+        className={`${typeBaseCN}--rail__bar`}
+        initial={{ width: 0 }}
+        animate={{ width: `${progressPercentage}%` }}
+        transition={{ duration: 0.35, ease: "easeOut" }}
+        aria-label={progressLabel}
+        data-progress={progressLabel}
+      ></motion.div>
     </div>
   );
 };
